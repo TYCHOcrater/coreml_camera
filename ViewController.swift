80682f5c7142737b743e65324bfe4591339197f9
TYCHOcrater <paketpaper@gmail.com>
2019-02-09 14:40:43 +0200

    Update ViewController.swift
    - changed few requests
    - added output (mostly from vision)

--- ViewController.swift
+++ ViewController.swift
@@ -7,18 +7,20 @@
 //
 
 import UIKit
-
 import AVKit
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
         //start up the camera
         
         let captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .photo
+        
+        
         guard let captureDevice =
             AVCaptureDevice.default(for: .video) else { return }
         guard let input = try? AVCaptureDeviceInput(device: captureDevice) else { return }
@@ -26,8 +28,28 @@ class ViewController: UIViewController {
         captureSession.addInput(input)
         
         captureSession.startRunning()
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        view.layer.addSublayer(previewLayer)
+        
+        previewLayer.frame = view.frame
+        
+        let dataOutput = AVCaptureVideoDataOutput()
+        dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+        
+        captureSession.addOutput(dataOutput)
+        
+ //       let request = VNCoreMLRequest(model: VNCoreMLModel, VNRequestCompletionHandler: VNRequestCompletionHandler)
+//     VNImageRequestHandler(cgImage: <#T##CGImage#>, options: [:]).perform(<#T##requests: [VNRequest]##[VNRequest]#>)
     }
-
+    
+    func captureOutput(_ output: AVCaptureOutput, didDrop sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        print("camera was able to capture a frame: ", Date())
+        
+        guard let pixelBuffer: CVPixelBuffer =  CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+        
+        VNImageRequestHandler(cvPixelBuffer: CVPixelBuffer, options: [:]).perform
+    }
 
 }
 
